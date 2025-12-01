1e33b6b458febdf5e8445de131904f97200beea4
Sebastian Avila Nunez <97568369+Tian2500@users.noreply.github.com>
2025-12-01 09:27:26 -0800

    Update config.js

--- config.js
+++ config.js
@@ -2,9 +2,9 @@ module.exports = {
 	helpCmdPerPage: 10, //- Number of commands per page of help command
 	lyricsMaxResults: 5, //- Number of results for lyrics command (Do not touch this value if you don't know what you are doing)
 	adminId: "UserId", //- Replace UserId with the Discord ID of the admin of the bot
-	token: process.env.token || "", //- Bot's Token
-	clientId: process.env.clientId || "", //- ID of the bot
-	clientSecret: process.env.clientSecret || "", //- Client Secret of the bot
+	token: process.env.token || "MTQ0NDg4ODk1OTgzNzgwMjU3OA.GTIQVd.ZMvzhldXdm8Y64BqAXwWs7x1JwaB8x1r69GLWQ", //- Bot's Token
+	clientId: process.env.clientId || "1444888959837802578", //- ID of the bot
+	clientSecret: process.env.clientSecret || "oJjXguaJ5Jv8ivSPVx6Yuk8LFmokj1ex", //- Client Secret of the bot
 	port: 4200, //- Port of the API and Dashboard
 	scopes: ["identify", "guilds", "applications.commands"], //- Discord OAuth2 Scopes
 	inviteScopes: ["bot", "applications.commands"], // Invite link scopes
@@ -26,9 +26,9 @@ module.exports = {
 	nodes: [
 		{
 			identifier: "Main Node", //- Used for indentifier in stats commands.
-			host: "", //- The host name or IP of the lavalink server.
-			port: 80, // The port that lavalink is listening to. This must be a number!
-			password: "", //- The password of the lavalink server.
+			host: "lavalink.jirayu.net", //- The host name or IP of the lavalink server.
+			port: 13592, // The port that lavalink is listening to. This must be a number!
+			password: "youshallnotpass", //- The password of the lavalink server.
 			retryAmount: 200, //- The amount of times to retry connecting to the node if connection got dropped.
 			retryDelay: 40, //- Delay between reconnect attempts if connection is lost.
 			secure: false, //- Can be either true or false. Only use true if ssl is enabled!
